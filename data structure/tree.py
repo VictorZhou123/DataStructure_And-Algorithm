e0917cf37f272bb4319d11d03db50d4f0addce02
用户名 <896370742@qq.com>
2021-08-30 00:15:31 +0800

    master

--- data structure/tree.py
+++ data structure/tree.py
@@ -11,3 +11,39 @@ class Node:
 class FileSystemTree:
     def __init__(self):
         self.root = Node("/")
+        self.now = self.root
+    
+    def mkdir(self, name):
+        if name[-1] != '/':
+            name += '/'
+        node = Node(name)
+        self.now.children.append(node)
+        node.parent = self.now
+    
+    def ls(self):
+        return self.now.children
+
+    def cd(self, name):
+        if name[-1] != '/':
+            name += '/'
+        if name == '../':
+            self.now = self.now.parent
+            return
+        for child in self.now.children:
+            if name == child.name:
+                self.now = child
+                return
+        raise ValueError("invalid dir")
+        
+        
+
+tree = FileSystemTree()
+tree.mkdir('var')
+tree.mkdir('usr')
+tree.mkdir('bin')
+print(tree.ls())
+tree.cd("bin")
+tree.mkdir('python')
+print(tree.ls())
+tree.cd("../")
+print(tree.ls())
